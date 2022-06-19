b312766d62e92ae0c5a8bcb18a28c3134bf36073
Rinat Saifullin <saifullinrg@gmail.com>
2022-06-19 11:25:40 +0500

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Сайфуллин Ринат on 19.06.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
